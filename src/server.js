fb9ace9b334f5ced56c554998d302998962893d9
Sourav Sharma <souravat@outlook.com>
2021-06-30 14:25:17 +0530

    Added socket.io ✔

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
+//import logger from "morgan";
+//const morgan = require('morgan');
 const path = require('path');
 const express = require('express');
-const socketIO = require('socketIO');
+const socketIO = require('socket.io');
 const app = express();
 const PORT = 4040;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'static')));
+//app.use(app.logger('dev'));
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.listen(PORT, () => {
+//saving into server variable for socket.io
+const server = app.listen(PORT, () => {
 	console.log('server is listening ✔ ');
 });
 
+const io = socketIO(server);
+
+io.on('connection', (socket) => console.log(socket));
